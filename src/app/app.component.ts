a094c6e693825744301643eb5b028fc1b7b1b642
TEO-INTL\Muhammad.Shahab <muhammad.shahab@teo-intl.com>
2024-02-29 09:30:55 +0500

    complete the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,41 @@
 import { NgClass, NgFor, NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgClass, NgFor, NgIf],
+  imports: [RouterOutlet, NgClass, NgFor, NgIf, ReactiveFormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   @ViewChild('mainNav') mainNav!: ElementRef;
-  images:string[] = []
+  images: string[] = []
   selectedService: number = 0;
+  contactForm!: FormGroup;
+  successMessage: string | undefined;
+  errorMessage: string | undefined;
+  loading: boolean = false
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
   ngOnInit(): void {
     this.setImagesArray()
+    this.setForm()
     this.shrinkNavbar();
     window.addEventListener('scroll', this.shrinkNavbar.bind(this));
     this.activateScrollspy();
     this.collapseResponsiveNavbar();
   }
-
+  setForm() {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
   shrinkNavbar() {
     if (!this.mainNav.nativeElement) {
       return;
@@ -82,11 +97,50 @@ export class AppComponent implements OnInit {
   setSelectedService(index: number) {
     this.selectedService = index
   }
-  setImagesArray(){
+  setImagesArray() {
     this.images = Array.from({ length: 46 }, (_, i) => `image_${i + 1}.jpg`);
-    console.log(this.images)
   }
-  getImageUrl(image: string): string {
-    return `../assets/img/success-patner/${image}`;
+  submitForm() {
+    if (this.contactForm.valid) {
+      this.loading = true
+      // this.successMessage = 'Form submission successful!';
+      // this.errorMessage = '';
+      const formData = new FormData();
+      formData.append('wpforms[fields][0][first]', this.contactForm.get('name')?.value);
+      formData.append('wpforms[fields][0][last]', '');
+      formData.append('wpforms[fields][1]', this.contactForm.get('email')?.value);
+      formData.append('wpforms[fields][2]', this.contactForm.get('message')?.value);
+      formData.append('wpforms[id]', '292');
+      formData.append('wpforms[author]', '2');
+      formData.append('wpforms[post_id]', '8');
+      formData.append('wpforms[submit]', 'wpforms-submit');
+      formData.append('wpforms[token]', 'ef8f630892fa9ccb788f4fe4936061a0');
+      formData.append('action', 'wpforms_submit');
+      formData.append('page_url', 'https://asalatalkhafji.com/');
+      formData.append('page_title', 'Home');
+      formData.append('page_id', '8');
+      formData.append('start_timestamp', '1709179962825');
+      formData.append('end_timestamp', '1709179974251');
+
+      this.http.post('https://asalatalkhafji.com/wp-admin/admin-ajax.php', formData)
+        .subscribe({
+          next: (response) => {
+            console.log('Form submission successful!', response);
+            this.successMessage = 'Form submission successful!';
+            this.errorMessage = '';
+            this.loading = false;
+          },
+          error: (error) => {
+            console.error('Error sending message!', error);
+            this.successMessage = '';
+            this.errorMessage = 'Error sending message!';
+            this.loading = false;
+          }
+        });
+
+    } else {
+      this.successMessage = '';
+      this.errorMessage = 'Error sending message!';
+    }
   }
 }
